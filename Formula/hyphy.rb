63f09866a409bd58e4409d61e6af02133642ace9
Steven Weaver <sweaver@temple.edu>
2019-05-23 13:23:33 -0400

    adding hyphy

--- Formula/hyphy.rb
+++ Formula/hyphy.rb
@@ -6,15 +6,6 @@ class Hyphy < Formula
   head "https://github.com/veg/hyphy.git"
   # tag "bioinformatics"
 
-  bottle do
-    sha256 "b19ac75cbe0ce6ca2eb4ebcfbe8cf9cda8f65fb230beb57eef2c8d77d75655ec" => :sierra
-    sha256 "74b0628be9dddccc04b1a33c80ac25512fcc27e5f713eb51c7175a4a1a65096f" => :el_capitan
-    sha256 "7a1525ce1e42fc359320dec513d9397b7d02190b9ce8bc0f892796494ca58db3" => :yosemite
-  end
-
-  option "with-opencl", "Build a version with OpenCL GPU/CPU acceleration"
-  option "without-multi-threaded", "Don't build a multi-threaded version"
-  option "without-single-threaded", "Don't build a single-threaded version"
   deprecated_option "with-mpi" => "with-open-mpi"
 
   depends_on "openssl"
@@ -25,7 +16,6 @@ class Hyphy < Formula
     system "cmake", "-DINSTALL_PREFIX=#{prefix}", ".", *std_cmake_args
     system "make", "MP"
     system "make", "MPI" if build.with? "open-mpi"
-    system "make", "OCL" if build.with? "opencl"
     system "make", "GTEST"
 
     system "make", "install"
